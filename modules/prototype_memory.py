80ff9b4f466323f0b2da29930f5affccb25c4ddf
Ryan Teehan <rsteehan@gmail.com>
2022-08-29 11:15:53 -0400

    avging

--- modules/prototype_memory.py
+++ modules/prototype_memory.py
@@ -4,30 +4,12 @@ import torch.functional as F
 
 class OnlineProtoNet(nn.Module):
 
-    def __init__(self, betas_r, gammas_r, betas_w, gammas_w, distance):
-
+    def __init__(self):
+        super(OnlineProtoNet, self).__init__()
         self.memory = {}
-        self.betas_r = betas_r
-        self.gammas_r = gammas_r
-        self.betas_w = betas_w
-        self.gammas_w = gammas_w
-        self.dist = distance
 
-
-    def forward(self):
-        pass
-
-    def avg(self, p_last, c_last, embed):
-        c = c_last + 1
-        next_val = (1 / c) @ (p_last @ c_last + embed)
-        return c, next_val
-
-    def get_novelty(self, t, input, embed):
-        dists = self.dist(embed, self.query(input))
-        return torch.sigmoid(torch.min(dists) - self.betas_w[t]/self.gammas_w[t])
-
-    # def storestore_example_example(self, x, embed, c_last):
-
-    def query(self, input):
-        pass
-
+    def store(self, word, embed):
+        if word not in self.memory:
+            self.memory[word] = (embed, 1)
+        else:
+            self.memory[word] = (torch.div(embed + self.memory[word][0], self.memory[word][1]), self.memory[word][1] + 1)
