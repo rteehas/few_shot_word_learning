920af8594147ac7a1079692832de521f580218ed
Ryan Teehan <rsteehan@gmail.com>
2022-09-19 22:12:36 -0400

    add masked attn seq2seq

--- modules/mlm.py
+++ modules/mlm.py
@@ -3,67 +3,25 @@ from torch import nn
 from functools import reduce
 
 
-# class BERTWrapper(nn.Module):
+class AttnSeq2Seq(nn.Module):
+    def __init__(self, tokenizer, dim):
+        super(AttnSeq2Seq, self).__init__()
+        self.embedding = nn.Embedding(len(tokenizer), dim)
+        # self.pos_encoder = PositionalEncoding(INPUT_DIM, dropout)
 
-#     def __init__(self, model, tokenizer):
-#         super(BERTWrapper, self).__init__()
-#         # assumes bert-style model (i.e. BERT or RoBERTa)
-#         self.model = model
-#         self.tokenizer = tokenizer
+        encoder_layer = nn.TransformerEncoderLayer(d_model=dim, nhead=1)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=1)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=dim, nhead=1)
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=1)
 
-#     def forward(self, inputs, nonce, **kwargs):
-#         # if kwargs:
-#         #     if kwargs["memory_aug"]:
-#         labels = inputs["input_ids"]
-#         # print(inputs.input_ids.size(),inputs.input_ids.shape)
-#         # ids = inputs.input_ids.unsqueeze(0)
-#         # attn_mask = inputs.attention_mask.unsqueeze(0)
-#         # print(ids.size(), ids.shape)
-#         # print(attn_mask.size())
+        self.linear = nn.Linear(dim, len(tokenizer))
+        self.softmax = nn.Softmax(dim=-1)
+        self.d = dim
 
-#         # input_embeds = inputs["input_embeds"]
-#         # labels[labels != self.tokenizer.mask_token_id] = -100
-
-#         outputs = self.model(input_ids=inputs.input_ids, attention_mask=inputs.attention_mask, output_hidden_states=True)
-
-#             # else:
-#             #     labels = inputs["input_ids"].clone()
-#             #     labels[labels != self.tokenizer.mask_token_id] = -100
-
-#             #     outputs = self.model(input_embeds=inputs["input_ids"],
-#             #                               labels=labels,
-#             #                               attn_mask=inputs["attention_mask"],
-#             #                               output_hidden_states=True)
-
-#         return outputs
-
-class MaskedAttention(nn.Module):
-
-    def __init__(self, tokenizer, embed_dim):
-        super(MaskedAttention, self).__init__()
-        self.tokenizer = tokenizer
-        self.morph = nn.TransformerEncoder(nn.TransformerEncoderLayer(embed_dim, 1),1).to(device)
-        self.d = embed_dim
-
-    def forward(self, input, encoded, seq, nonce):
-        span = self.calc_nonce_span(nonce, encoded, seq)
-        mask = self.mask_nonce(span, encoded)
-        tmp_input = input.clone()
-        msk_dim = mask.size()[1]
-        # tmp_input = tmp_input * mask
-        return self.morph(tmp_input, mask=torch.reshape(mask, (msk_dim,1,1)))
-
-    # def calculate_spans(self, encoded):
-    #     # taken from https://github.com/huggingface/tokenizers/issues/447
-    #     desired_output = []
-    #     for word_id in encoded.words():
-    #         if word_id is not None:
-    #             start, end = encoded.word_to_tokens(word_id)
-    #             word = self.tokenizer.decode(encoded[start:end])
-    #             new_tok_name = word + "_merged"
-    #             desired_output.append((start, end, new_tok_name))
-
-    #     return desired_output
+    def get_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
 
     def calc_nonce_span(self, nonce, encoded, seq):
         for word_id in encoded.words():
@@ -72,21 +30,37 @@ class MaskedAttention(nn.Module):
                 s, e = encoded.word_to_chars(word_id)
                 # print(seq[s:e+1])
                 if seq[s:e] == nonce:
-                  return (start, end)
-
-
-    # def calculate_masks(self, spans):
-    #     broken_words = filter(lambda tup: abs(tup[1] - tup[0] > 1), spans)
-    #     masks = []
-    #     for span in broken_words:
-    #         maskless = torch.full_like(len(spans), False, dtype=torch.bool)
-    #         masked = maskless.index_fill(int(0), [i for i in range(span[0], span[1])], True)
-    #         masks.append((masked, span[2]))
-    #     return masks
+                    return (start, end)
 
     def mask_nonce(self, span, encoded):
-        maskless = torch.full_like(enc.input_ids, False, dtype=torch.bool)
+        maskless = torch.full_like(encoded.input_ids, False, dtype=torch.bool)
         for i in range(span[0], span[1]):
-          maskless[0][i] = True
+            maskless[0][i] = True
         # masked = maskless.index_fill(0, torch.Tensor([i for i in range(span[0], span[1])]), 1)
         return ~maskless
+
+    def extract_nonce_embeds(self, span, embeds):
+        new_embeds = torch.zeros_like(embeds)
+        diff = span[1] - span[0]
+        new_embeds[:, :diff] = embeds[:, span[0]:span[1]]  # assumes embeds shape [Batch, Seq, Embed Dim]
+        return new_embeds
+
+    def forward(self, src, tgt, src_key_padding_mask=None, tgt_key_padding_mask=None):
+        # src = self.embedding(src)
+        # src = self.pos_encoder(src)
+        src = self.transformer_encoder(src, src_key_padding_mask=src_key_padding_mask)
+
+        tgt_mask = self.get_mask(tgt.size(0)).to(device)
+        tgt = self.embedding(tgt)
+        # tgt = self.pos_encoder(tgt)
+
+        output = self.transformer_decoder(
+            tgt=tgt,
+            memory=src,
+            tgt_mask=tgt_mask,  # to avoid looking at the future tokens (the ones on the right)
+            tgt_key_padding_mask=tgt_key_padding_mask,  # to avoid working on padding
+            memory_key_padding_mask=src_key_padding_mask  # avoid looking on padding of the src
+        )
+
+        # output = self.linear(output)
+        return output
