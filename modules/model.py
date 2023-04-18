62c18b6a872195c4e960bf3ef2cfecbdaef80282
Ryan Teehan <rsteehan@gmail.com>
2023-04-18 14:59:09 -0400

    update memory

--- modules/model.py
+++ modules/model.py
@@ -9,25 +9,35 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class OnlineProtoNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, agg="mean"):
         super(OnlineProtoNet, self).__init__()
+        self.agg_method = agg
         self.memory = {}
+        if agg == "CLS":
+            input_size = 768
+            nhead = 8
+            num_layers = 2
+            hidden_size = 512
+            num_positions = 25
+            self.agg = TransformerSummarizerWithCLS(input_size, nhead, num_layers, hidden_size, num_positions)
+        elif agg == "RNN":
+            self.agg = BiGRUSummarizer(768, 384)
 
     def store(self, word, embed):
         if word not in self.memory:
-            self.memory[word] = (embed, 1)
+            self.memory[word] = [embed]
         else:
-            self.memory[word] = (torch.div(embed + self.memory[word][0], self.memory[word][1] + 1), self.memory[word][1] + 1)
+            #             print(embed.shape, self.memory[word][0].shape, self.memory[word][0].requires_grad)
+            self.memory[word].append(embed)
 
     def retrieve(self, word):  # retrieves embed after linear layer to fix dims if needed
-        return self.memory[word][0]
+        ctx = torch.cat(self.memory[word], dim=0)
+        if self.agg_method == "mean":
 
-
-def init_new_dim(size, method="random"):
-    if method == "zeros":
-        return torch.zeros(size)
-    elif method == "random":
-        return torch.rand(size)
+            #             print(ctx.requires_grad, "ctx")
+            return torch.mean(ctx, dim=0)
+        else:
+            return self.agg(ctx.unsqueeze(0))
 
 
 class MorphMemoryModelAutoregressive(nn.Module):
@@ -304,6 +314,53 @@ class MorphMemoryModel(nn.Module):
         with torch.no_grad():
             toks = self.tokenizer(definition, return_tensors="pt").to(device)
             outs = self.firstLM(**toks, output_hidden_states=True)
+
+class BiGRUSummarizer(nn.Module):
+    # batch, seq, hidden
+    def __init__(self, input_size, hidden_size):
+        super(BiGRUSummarizer, self).__init__()
+        self.bigru = nn.GRU(input_size, hidden_size, batch_first=True, bidirectional=True)
+
+    def forward(self, token_embeddings):
+        _, hidden = self.bigru(token_embeddings)
+        fwd_hidden = hidden[0:1]  # Forward hidden state
+        bwd_hidden = hidden[1:2]  # Backward hidden state
+        output_embedding = torch.cat((fwd_hidden, bwd_hidden), dim=2).squeeze(0)
+        return output_embedding
+
+class TransformerSummarizerWithCLS(nn.Module):
+    def __init__(self, input_size, nhead, num_layers, hidden_size, num_positions):
+        super(TransformerSummarizerWithCLS, self).__init__()
+        self.embedding = nn.Embedding(num_positions, input_size)
+        self.cls_embedding = nn.Parameter(torch.randn(1, 1, input_size))
+        self.pos_encoder = self.positional_encoding(input_size, num_positions + 1)  # +1 for the [CLS] token
+        encoder_layers = TransformerEncoderLayer(input_size, nhead, hidden_size)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers)
+
+    def forward(self, token_embeddings):
+        batch_size = token_embeddings.shape[0]
+        cls_token = self.cls_embedding.expand(batch_size, -1, -1)  # Create [CLS] tokens for each item in the batch
+        token_embeddings = torch.cat([cls_token, token_embeddings], dim=1)  # Prepend [CLS] tokens to the input
+
+        seq_len = token_embeddings.shape[1]
+        pos = torch.arange(seq_len, dtype=torch.long).unsqueeze(0)
+        pos_embeddings = self.embedding(pos)
+        token_embeddings = token_embeddings + pos_embeddings
+
+        token_embeddings = token_embeddings.transpose(0, 1)  # TransformerEncoder expects (S, N, E) format
+        output = self.transformer_encoder(token_embeddings)
+        cls_embedding = output[0]  # [CLS] embedding is at position 0
+        return cls_embedding
+
+    @staticmethod
+    def positional_encoding(input_size, num_positions):
+        pe = torch.zeros(num_positions, input_size)
+        position = torch.arange(0, num_positions, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, input_size, 2).float() * (-torch.log(torch.tensor(10000.0)) / input_size))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        return nn.Parameter(pe, requires_grad=False)
 
 
 class MetaLearner(nn.Module):
