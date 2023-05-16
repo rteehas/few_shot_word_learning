0be5624b5267b2185447d811ebf8c69eed483095
Ryan Teehan <rsteehan@gmail.com>
2023-05-16 00:34:41 -0400

    add model utils

--- modules/utils.py
+++ modules/utils.py
@@ -1,22 +1,6 @@
 import torch
 import numpy as np
 
-def get_tgt_mask(size):
-    # Generates a squeare matrix where the each row allows one word more to be seen
-    mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
-    mask = mask.float()
-    mask = mask.masked_fill(mask == 0, float('-inf'))  # Convert zeros to -inf
-    mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
-
-    # EX for size=5:
-    # [[0., -inf, -inf, -inf, -inf],
-    #  [0.,   0., -inf, -inf, -inf],
-    #  [0.,   0.,   0., -inf, -inf],
-    #  [0.,   0.,   0.,   0., -inf],
-    #  [0.,   0.,   0.,   0.,   0.]]
-
-    return mask
-
 
 def get_word_idx(sent, word):
     return sent.split(" ").index(word)
@@ -24,7 +8,6 @@ def get_word_idx(sent, word):
 
 def combine_layers(hidden_states, layers):
     return torch.stack([hidden_states[i] for i in layers]).sum(0).squeeze()
-
 
 def extract_word_embeddings(hidden_states, layers, word_index, encoded):
     tp = torch.stack([hidden_states[i] for i in layers]).sum(0).squeeze()
