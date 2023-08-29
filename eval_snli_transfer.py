f867aceda2e791da21d65638c204a09741fbc756
Ryan Teehan <rteehan@uchicago.edu>
2023-08-29 10:18:17 -0400

    add print

--- eval_snli_transfer.py
+++ eval_snli_transfer.py
@@ -96,6 +96,7 @@ def main():
     #                                   tokenizerMLM.mask_token_id, memory_config, emb_gen='Transformer').to(device)
 
     path = "model_checkpoints/wd0.1_resample_False__redo_full_gelu_online_6examples_0.003_mean_Transformer_bs8_modified_mamlFalse_randomTrue_finetuneFalse_cat_Falselayers4_binary_False_mask_newTrue/MLMonline_memory_model_roberta_roberta_mean_memory_NormedOutput/checkpoints/"
+    print(path)
     split = dataset.train_test_split(0.2)
     for trial in range(5):
         for i in range(args.start_checkpoint, args.end_checkpoint + 1):
