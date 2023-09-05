d6492df51b861f8440cfc20162347e7de1b38270
Ryan Teehan <rsteehan@gmail.com>
2023-09-04 21:06:54 -0400

    add logging for eval

--- run_semantics.py
+++ run_semantics.py
@@ -773,6 +773,12 @@ def main():
         result = evaluate(args, model, reader, tokenizer, prefix=global_step, accelerator=accelerator)
         result = dict((k + '_{}'.format(global_step), v) for k, v in result.items())
         results.update(result)
+        eval_log_dict = {}
+        for key, value in results.items():
+            eval_log_dict['eval_{}'.format(key)] = value
+            # eval_log_dict['epoch'] = epoch
+
+        accelerator.log(eval_log_dict)
 
 
     # Evaluation
