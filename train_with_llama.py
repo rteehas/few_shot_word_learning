7c8d5d5501cc791411c06f00c2b29aefd2e04eea
Ryan Teehan <rsteehan@gmail.com>
2023-09-17 17:12:32 -0400

    add generation stuff

--- train_with_llama.py
+++ train_with_llama.py
@@ -27,6 +27,56 @@ from accelerate import DistributedDataParallelKwargs
 import psutil
 
 
+def decoding_step(logits, temperature, top_k=None, do_sample=False):
+    scaled_logits = logits[:, -1, :] / temperature
+    if top_k is not None:
+        v, _ = torch.topk(scaled_logits, min(top_k, scaled_logits.size(-1)))
+        scaled_logits[scaled_logits < v[:, [-1]]] = -float('Inf')
+
+    probs = F.softmax(scaled_logits, dim=-1)
+    if do_sample:
+        idx_next = torch.multinomial(probs, num_samples=1)
+    else:
+        idx_next = torch.argmax(probs, keepdim=True)
+
+    return idx_next
+
+
+@torch.no_grad
+def generate(model, context, input_ids, attention_mask, max_new_tokens, temperature=1.0, top_k=None):
+    initial_batch = {
+        "contexts": [context],
+        'input_ids': input_ids,
+        'attention_mask': attention_mask,
+        'labels': input_ids.clone()
+    }
+    initial_outputs = model(initial_batch)
+
+    input_weights = model.get_new_weights(task="Task", memory=initial_outputs.memories[0]['input_memory'])
+    output_weights = model.get_new_output_weights(initial_outputs.memories[0]['output_weight'])
+
+    first_token = decoding_step(initial_outputs.logits, temperature, top_k)
+    new_input_ids = torch.cat([input_ids, first_token], dim=1)
+    last_element = attention_mask[:, -1].unsqueeze(1)
+    new_attention_mask = torch.cat([attention_mask, last_element], dim=1)
+
+    for i in range(1, max_new_tokens):
+        input_embeds = F.embedding(new_input_ids, input_weights)
+        outputs = model.secondLM.model(
+            inputs_embeds=input_embeds,
+            attention_mask=new_attention_mask
+        )
+        llama_outputs = model.llama_forward(labels=None, outputs=outputs, new_w=output_weights)
+
+        next_token = decoding_step(llama_outputs.logits, temperature, top_k, do_sample)
+        #         print(next_token.shape)
+        #         print(new_input_ids.shape)
+        new_input_ids = torch.cat([new_input_ids, next_token], dim=1)
+        last_element = new_attention_mask[:, -1].unsqueeze(1)
+        new_attention_mask = torch.cat([new_attention_mask, last_element], dim=1)
+
+    return new_input_ids
+
 class EmbeddingGenerator(nn.Module):
 
     def __init__(self, firstLM, secondLM):
