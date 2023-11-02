5a9caece0e03d2fffbc9251ae2e683a56be7138a
Ryan Teehan <rsteehan@gmail.com>
2023-11-02 13:16:21 -0400

    typo calculating curr step

--- train_with_llama.py
+++ train_with_llama.py
@@ -1211,8 +1211,13 @@ def main():
         base_epoch = int(num1)
         step = int(num2) + 1 # correct for 0 first step
         assert step % args.gradient_accumulation_steps == 0, "Choose a checkpoint corresponding to a gradient update"
-        curr_global_step = base_epoch * len(train_dl) // args.gradient_accumulation_steps
-        curr_neg_step = base_epoch * len(negative_train_dl) // args.gradient_accumulation_steps
+        if base_epoch != 0:
+            curr_global_step = (step // (base_epoch * len(train_dl))) // args.gradient_accumulation_steps
+            curr_neg_step = (step //(base_epoch * len(negative_train_dl))) // args.gradient_accumulation_steps
+        else:
+            curr_global_step = step // args.gradient_accumulation_steps
+            curr_neg_step = step // args.gradient_accumulation_steps
+
         train_dl = accelerator.skip_first_batches(train_dl, curr_global_step)
         if args.negative_examples:
             negative_train_dl = accelerator.skip_first_batches(negative_train_dl, curr_neg_step)
