0165a0e4d68e8f729bc90941ba42ae23bc6713a9
Ryan Teehan <rteehan@uchicago.edu>
2023-11-27 10:28:42 -0500

    update path

--- train_with_llama.py
+++ train_with_llama.py
@@ -877,8 +877,13 @@ def create_checkpoint_directories(args):
     else:
         neg_string = "without_negatives_or_regression"
 
-    path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/pile/layernorm/{}_layers/last_{}/{}_batch_size/{}_agg/{}_examples/lr_{}/weight_decay_{}/{}/"
-    path = path.format(args.num_layers, args.num_feature_layers, args.batch_size * args.gradient_accumulation_steps, args.memory,
+    if "interleaved" in args.data_path:
+        dataset_name = "interleaved"
+    else:
+        dataset_name= "pile"
+
+    path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/{}/layernorm/{}_layers/last_{}/{}_batch_size/{}_agg/{}_examples/lr_{}/weight_decay_{}/{}/"
+    path = path.format(dataset_name, args.num_layers, args.num_feature_layers, args.batch_size * args.gradient_accumulation_steps, args.memory,
                        args.num_examples, args.lr, args.weight_decay, neg_string)
 
     if args.negative_examples and args.regression_objective:
