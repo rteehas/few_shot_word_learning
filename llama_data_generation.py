5838b959e957c02d08eb90c050167d625cd893a1
Ryan Teehan <rteehan@uchicago.edu>
2023-12-02 01:13:06 -0500

    wip

--- llama_data_generation.py
+++ llama_data_generation.py
@@ -38,18 +38,17 @@ def main():
     data = load_from_disk(args.data_path)
     data = data.map(get_prefixes)
 
-    orig_dset_name = data.split("/")[-1]
-
+    orig_dset_name = args.data_path.split("/")[-1]
+    distributed_state = PartialState()
     model = LlamaForCausalLM.from_pretrained("/vast/work/public/ml-datasets/llama-2/Llama-2-7b-hf",
-                                                low_cpu_mem_usage=True)
+                                                low_cpu_mem_usage=True, device_map=distributed_state.device)
     tokenizer = LlamaTokenizer.from_pretrained("/vast/work/public/ml-datasets/llama-2/Llama-2-7b-hf", legacy=True,
                                    use_fast=False)
 
-    distributed_state = PartialState()
 
     prefixes = list(set(data['train']['prefix'] + data['test']['prefix']))
 
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer).to(distributed_state.device)
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device=distributed_state.device)
 
     with distributed_state.split_between_processes(prefixes) as partial_prefixes:
 
