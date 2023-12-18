0ea1e2b3aa6cc817f9962b5e2fcee97e4456d0c1
Ryan Teehan <rteehan@uchicago.edu>
2023-12-18 01:53:48 -0500

    change beta

--- train_with_llama.py
+++ train_with_llama.py
@@ -1285,7 +1285,7 @@ def main():
     eval_ind = args.logging_step
 
     opt = AdamW(optimizer_grouped_parameters,
-                betas=(0.9,0.95),
+                betas=(0.9,0.98),
                 eps=epsilon,
                 lr=lr,
                 weight_decay=args.weight_decay
