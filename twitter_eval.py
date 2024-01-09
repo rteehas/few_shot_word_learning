1ed1e965a25e895cd3f15a23d0ebeb456b3ac1ec
Ryan Teehan <rsteehan@gmail.com>
2024-01-09 08:14:59 -0500

    wip

--- twitter_eval.py
+++ twitter_eval.py
@@ -9,21 +9,58 @@ device = "cuda"
 example_prompt = "The following are examples using a new word <nonce>:\n{}\nThe definition of this word is \"{}\". The word is <nonce>"
 def_prompt = "The definition of a new word is \"{}\". The word is <nonce>"
 
-def prepare_example(ex, k):
+def prepare_example(ex, k, emb_gen):
     samples = []
     seqs = []
     labels = [1,0,0,0]
     word = ex['word']
-    word_samples = np.random.choice([s.replace(word, "<nonce>") for s in ex['word_examples']], size=k, replace=False).tolist()
+    if word == "take the l":
+        word = "take the L"
+    if word == "goblin era":
+        word = "goblin mode"
+    if word == "menty b":
+        word = "menty B"
+    if word == "caught in 4k":
+        word = "caught in 4K"
+    if word == "trade":
+        word = "trad"
+    word_samples = np.random.choice([s.replace(word, "<nonce>").replace(word.lower(), "<nonce>").replace(word.capitalize(), "<nonce>").replace(word.upper(), "<nonce>").replace(" ".join([w.capitalize() for w in word.split(" ")]), "<nonce>") for s in ex['word_examples'] if word.lower() in s.lower()], size=k, replace=False).tolist()
+    if word == "beyhive":
+        word_samples = [s.replace("BeyHive", "<nonce>") for s in word_samples]
+    if word == "goblin mode":
+        word_samples = [s.replace("GOBLIN mode", "<nonce>") for s in word_samples]
     definition = ex['definition'].replace(word, "<nonce>")
-    word_seq = example_prompt.format("\n".join(word_samples), definition)
+    if emb_gen:
+        word_seq = def_prompt.format(definition)
+    else:
+        word_seq = example_prompt.format("\n".join(word_samples), definition)
+    print("word seq", word_seq)
     seqs.append(word_seq)
     samples.append(word_samples)
     # new_ex['word'] = word
     for i in range(3):
         neg = ex["negative_choice_{}".format(i)]
-        neg_samples = np.random.choice([s.replace(neg, "<nonce>") for s in ex['negative_choice_examples_{}'.format(i)]], size=k, replace=False)
-        neg_seq = example_prompt.format("\n".join(neg_samples), definition)
+        if neg == "take the l":
+            neg = "take the L"
+        if neg == "goblin era":
+            neg = "goblin mode"
+        if neg == "menty b":
+            neg = "menty B"
+        if neg == "caught in 4k":
+            neg = "caught in 4K"
+        if neg == "trade":
+            neg = "trad"
+
+        neg_samples = np.random.choice([s.replace(neg, "<nonce>").replace(neg.lower(), "<nonce>").replace(neg.capitalize(), "<nonce>").replace(neg.upper(), "<nonce>").replace(" ".join([w.capitalize() for w in neg.split(" ")]), "<nonce>") for s in ex['negative_choice_examples_{}'.format(i)] if neg.lower() in s.lower()], size=k, replace=False).tolist()
+        if neg == "beyhive":
+            neg_samples = [s.replace("BeyHive", "<nonce>") for s in neg_samples]
+        if neg == "goblin mode":
+            neg_samples = [s.replace("GOBLIN mode", "<nonce>") for s in neg_samples]
+        if emb_gen:
+            neg_seq = def_prompt.format(definition)
+        else:
+            neg_seq = example_prompt.format("\n".join(neg_samples), definition)
+        print("neg seq", neg_seq)
         seqs.append(neg_seq)
         samples.append(neg_samples)
     return samples, seqs, labels
@@ -110,11 +147,11 @@ def evaluate_type_1(probs, labels):
     return max_prob == lab_id
 
 def get_sentence_prob(labels, logits, model):
-    ce = CrossEntropyLoss()
+    ce = CrossEntropyLoss(reduction='none')
     # print(answer_labels)
     shift_logits = logits[..., :-1, :].contiguous()
     shift_labels = labels[..., 1:].contiguous()
-    shift_logits = shift_logits.view(-1, model.config.vocab_size)
+    shift_logits = shift_logits.view(-1, model.config.vocab_size + 1)
     shift_labels = shift_labels.view(-1)
     shift_labels = shift_labels.to(shift_logits.device)
     loss = ce(shift_logits, shift_labels)
@@ -125,7 +162,8 @@ def evaluate_example_emb_gen(ex, model, tokenizerMLM, tokenizerTask, k):
     samples, seqs, labels = prepare_example(ex, k)
     probs = []
     for sample, seq in zip(samples, seqs):
-        ctx = tokenizerMLM(samples, truncation=True, padding='longest', return_tensors='pt').to(device)
+        print(sample)
+        ctx = tokenizerMLM(sample, truncation=True, padding='longest', return_tensors='pt').to(device)
         input = tokenizerTask(seq,truncation=True, return_tensors='pt').to(device)
         batch = {
             'contexts': [ctx],
@@ -135,7 +173,7 @@ def evaluate_example_emb_gen(ex, model, tokenizerMLM, tokenizerTask, k):
         }
 
         outputs = model(batch)
-        prob = get_sentence_prob(input['input_ids'].clone(), outputs.logits)
+        prob = get_sentence_prob(input['input_ids'].clone(), outputs.logits, model.secondLM)
         probs.append(prob)
     return evaluate_type_1(probs, labels)
 
