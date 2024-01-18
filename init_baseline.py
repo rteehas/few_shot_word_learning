3fabf36bcc1b301d9c8efc7da1d8f12226491b05
Ryan Teehan <rsteehan@gmail.com>
2024-01-18 01:00:18 -0500

    add init

---
+++ init_baseline.py
@@ -0,0 +1,48 @@
+import torch
+from torch.distributions import MultivariateNormal
+
+
+
+def zero_init(model, tokenizer, new_tokens):
+    tokenizer.add_tokens(new_tokens)
+    model.resize_token_embeddings(len(tokenizer))
+    with torch.no_grad():
+        model.get_input_embeddings().weight[-len(new_tokens):, :] = 0.
+        model.get_output_embeddings().weight[-len(new_tokens):, :] = 0.
+    return model, tokenizer
+
+def mean_init(model, tokenizer, new_tokens):
+    with torch.no_grad():
+        input_mean = model.get_input_embeddings().weight.mean(dim=0)
+        output_mean = model.get_output_embeddings().weight.mean(dim=0)
+
+    tokenizer.add_tokens(new_tokens)
+    model.resize_token_embeddings(len(tokenizer))
+    with torch.no_grad():
+        model.get_input_embeddings().weight[-len(new_tokens):, :] = input_mean
+        model.get_output_embeddings().weight[-len(new_tokens):, :] = output_mean
+    return model, tokenizer
+
+def random_mean_init(model, tokenizer, new_tokens):
+    with torch.no_grad():
+        input_mean = model.get_input_embeddings().weight.mean(dim=0)
+        output_mean = model.get_output_embeddings().weight.mean(dim=0)
+        # input_cov = torch.cov(model.get_input_embeddings().weight)
+        # output_cov = torch.cov(model.get_output_embeddings().weight)
+
+    tokenizer.add_tokens(new_tokens)
+    model.resize_token_embeddings(len(tokenizer))
+
+    with torch.no_grad():
+        for tok in new_tokens:
+            cov = torch.eye(model.get_input_embeddings().weight.shape[1], device=model.device)
+            idx = tokenizer.convert_tokens_to_ids(tok)
+            model.get_input_embeddings().weight[idx, :] = MultivariateNormal(input_mean, cov).sample()
+            model.get_output_embeddings().weight[idx, :] = MultivariateNormal(output_mean, cov).sample()
+
+    return model, tokenizer
+
+def default_init(model, tokenizer, new_tokens):
+    tokenizer.add_tokens(new_tokens)
+    model.resize_token_embeddings(len(tokenizer))
+    return model, tokenizer
