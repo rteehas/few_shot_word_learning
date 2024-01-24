335d90ea5c7a1bd1a66d0c2969f187e07afb0559
Ryan Teehan <rsteehan@gmail.com>
2024-01-24 00:13:22 -0500

    fix hice, add additive

--- w2v_baselines.py
+++ w2v_baselines.py
@@ -10,6 +10,7 @@ from modules.model_outputs import CausalLMOutputWithNewToken
 from train_with_llama import decoding_step, Memory
 # from HiCE.model import HiCE
 from HiCE.util import load_training_corpus
+from HiCE.util import pad_sequences
 from gensim.models import Word2Vec
 import sys
 
@@ -37,7 +38,11 @@ class HiCEBaseline(nn.Module):
             output_memory = Memory()
             c = contexts[i].to(self.secondLM.device).unsqueeze(0)
             if "character" in batch:
-                vocab = batch['character'][i].unsqueeze(0)
+                if len(batch['character'][i].shape) == 1:
+                    vocab = batch['character'][i].unsqueeze(0)
+                else:
+                    vocab = batch['character'][i]
+
 
             pred_embed = self.hice(c, vocab)
             tok_input_embed = self.input_linear(pred_embed)
@@ -62,7 +67,7 @@ class HiCEBaseline(nn.Module):
         outs = []
         for tok_output in output_embeds:
             output_weights = self.get_new_output_weights(tok_output)
-            llama_outputs, new_tok_loss = self.llama_forward(task_labels[i], outputs, output_weights,
+            llama_outputs = self.llama_forward(task_labels[i], outputs, output_weights,
                                                              i, new_token_loss=False)
 
             out_vals = CausalLMOutputWithNewToken(
@@ -168,12 +173,155 @@ class HiCEBaseline(nn.Module):
 
 
 class AdditiveBaseline(nn.Module):
-    def __init__(self, w2v, input_linear_path, output_linear_path, secondLM):
+    def __init__(self, w2v, dictionary, input_linear_path, output_linear_path, secondLM):
         super().__init__()
         self.w2v = w2v
+        self.dictionary = dictionary
         self.input_linear = torch.load(input_linear_path)
         self.output_linear = torch.load(output_linear_path)
         self.secondLM = secondLM
+
+    def forward(self, batch):
+        contexts = batch['contexts']
+        output_embeds = []
+        task_ids = batch['input_ids']
+        task_attn = batch["attention_mask"]
+        task_labels = batch['labels']
+        task_input_embeds = []
+        mem_embeds = []
+        for i in range(len(contexts)):
+            input_memory = Memory()
+            output_memory = Memory()
+            c = contexts[i].to(self.secondLM.device).unsqueeze(0)
+
+
+            pred_embed = get_w2v_ctx_prediction(c, self.dictionary, pad=0)[0]
+            tok_input_embed = self.input_linear(pred_embed)
+            tok_output_embed = self.output_linear(pred_embed)
+            new_w = self.get_new_input_weights(tok_input_embed)
+            input_embeds = F.embedding(task_ids[i], new_w)
+            task_input_embeds.append(input_embeds)
+            output_embeds.append(tok_output_embed)
+            new_token = self.secondLM.config.vocab_size
+            input_memory.store(new_token, tok_input_embed)
+            output_memory.store(new_token, tok_output_embed)
+            mem_embeds.append(dict(input_memory=input_memory, output_memory=output_memory))
+
+        input_embeds = torch.stack(task_input_embeds)
+        attn = task_attn
+
+        outputs = self.secondLM.model(
+            inputs_embeds=input_embeds,
+            attention_mask=attn,
+            # output_hidden_states=True
+        )
+        outs = []
+        for tok_output in output_embeds:
+            output_weights = self.get_new_output_weights(tok_output)
+            llama_outputs = self.llama_forward(task_labels[i], outputs, output_weights,
+                                                             i, new_token_loss=False)
+
+            out_vals = CausalLMOutputWithNewToken(
+                loss=llama_outputs.loss,
+                logits=llama_outputs.logits,
+                past_key_values=None,
+                hidden_states=None,
+                attentions=None,
+                new_token_loss=None,
+                memories=[None]
+            )
+            outs.append(out_vals)
+
+        final_loss = torch.stack([o.loss for o in outs]).mean()
+        final_logits = torch.cat([o.logits for o in outs if o.logits is not None], dim=0)
+        return CausalLMOutputWithNewToken(
+            loss=final_loss,
+            logits=final_logits,
+            hidden_states=None,
+            attentions=None,
+            past_key_values=None,
+            new_token_loss=None,
+            memories=mem_embeds
+        )
+
+    def llama_forward(self, labels, outputs, new_w, index, new_token_loss=False):
+        '''
+        Copied from https://github.com/huggingface/transformers/blob/18ee1fe76295239335bf1528c744fe1cfba21cc8/src/transformers/models/llama/modeling_llama.py#L742C7-L742C7
+        Note: Output layer weights are not tied to word embedding weights https://github.com/facebookresearch/llama/issues/138
+        :param labels:
+        :param outputs:
+        :return:
+        '''
+        if index is not None:
+            hidden_states = outputs[0][index, :, :]
+        else:
+            hidden_states = outputs[0]
+        if self.secondLM.config.pretraining_tp > 1:
+            lm_head_slices = new_w.split(self.secondLM.vocab_size // self.secondLM.config.pretraining_tp, dim=0)
+            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.secondLM.config.pretraining_tp)]
+            logits = torch.cat(logits, dim=-1)
+        else:
+            logits = F.linear(hidden_states, new_w, bias=self.secondLM.lm_head.bias)
+        logits = logits.float()
+        # print(logits.shape, "logits")
+        # print(self.secondLM.lm_head.weight.shape, "lm_logits")
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, new_w.shape[0])
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+            if new_token_loss:
+                nt_loss_fct = CrossEntropyLoss(reduction='none')
+                non_reduced_loss = nt_loss_fct(shift_logits, shift_labels)
+                selected = non_reduced_loss[torch.where(torch.isin(torch.flatten(shift_labels), torch.tensor(self.second_list,
+                                                                            device=shift_logits.device).unique()))]
+                if selected.numel() > 0:
+                    new_token_loss = selected.mean()
+                else:
+                    new_token_loss = None
+
+                return CausalLMOutputWithPast(
+                    loss=loss,
+                    logits=logits,
+                    past_key_values=outputs.past_key_values,
+                    hidden_states=outputs.hidden_states,
+                    attentions=outputs.attentions,
+                ), new_token_loss
+            else:
+                return CausalLMOutputWithPast(
+                    loss=loss,
+                    logits=logits,
+                    past_key_values=outputs.past_key_values,
+                    hidden_states=outputs.hidden_states,
+                    attentions=outputs.attentions,
+                )
+
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+    def get_new_input_weights(self, new_input_embed):
+
+        input_w = self.secondLM.get_input_embeddings().weight
+        # output_w = self.secondLM.get_output_embeddings().weight
+
+        return torch.cat([input_w, new_input_embed])
+
+    def get_new_output_weights(self, new_output_embed):
+        output_w = self.secondLM.get_output_embeddings().weight
+        return torch.cat([output_w, new_output_embed])
+
 
 class Nonce2VecBaseline(nn.Module):
     def __init__(self, n2v, input_linear_path, output_linear_path, secondLM):
@@ -184,7 +332,7 @@ class Nonce2VecBaseline(nn.Module):
         self.secondLM = secondLM
 
 
-def make_hice_batch(contexts, true_word, maxlen = 24, pad=0):
+def make_hice_batch(contexts, true_word, dictionary, maxlen = 24, pad=0):
     _vocab = {v: i + 1 for v, i in zip('abcdefghijklmnopqrstuvwxyz', range(26))}
     lefts, rights = [], []
     nonce = "<nonce>"
@@ -196,8 +344,8 @@ def make_hice_batch(contexts, true_word, maxlen = 24, pad=0):
         sent_without_nonce = " ".join([s for s in sent.split() if nonce not in s])
         toks = dictionary.sent2idx(sent_without_nonce)
         data['contexts'].append(toks)
-    data['character'] = [_vocab[v] for v in true_word if v in _vocab]
-    data['contexts'] = pad_sequences(data['contexts'], maxlen=maxlen, value=pad, padding='post', truncating='post')
+    data['character'] = torch.LongTensor(pad_sequences([[_vocab[v] for v in true_word if v in _vocab]], maxlen=maxlen))
+    data['contexts'] = torch.LongTensor(pad_sequences(data['contexts'], maxlen=maxlen, value=pad, padding='post', truncating='post'))
     return data
 
 def make_w2v_batch(contexts, pad=0):
@@ -211,8 +359,8 @@ def make_w2v_batch(contexts, pad=0):
         data['contexts'].append(toks)
     return data
 
-def get_w2v_ctx_prediction(context):
-    pred = [np.average([dictionary.idx2vec[pi] for pi in context if pi != pad], axis=0)]
+def get_w2v_ctx_prediction(oov_cxt, dictionary, pad=0):
+    pred = [torch.mean(torch.stack([dictionary.idx2vec[pi] for pi in pp if pi != pad]), dim=0) for pp in oov_cxt.reshape(oov_cxt.shape[0], -1)]
     return pred
 
 @torch.no_grad
