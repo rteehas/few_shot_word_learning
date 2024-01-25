d500f7b8627710fadfe6e2f949c72450b4dae726
Ryan Teehan <rsteehan@gmail.com>
2024-01-25 15:53:06 -0500

    def eval with temp

--- definition_eval.py
+++ definition_eval.py
@@ -27,7 +27,7 @@ def generate_definitions_emb_gen(model, ex, k, tokenizerMLM, tokenizerTask, with
 
     inputs = tokenizerTask(prompt, truncation=True, return_tensors='pt', max_length=256).to(device)
 
-    outputs = generate(model, context, inputs['input_ids'], inputs['attention_mask'], 30, temperature=1.0, top_k=None, do_sample=False, mask_new_tokens=True)
+    outputs = generate(model, context, inputs['input_ids'], inputs['attention_mask'], 30, temperature=0.8, top_k=10, do_sample=True, mask_new_tokens=True)
     # print(outputs)
     generated_def = tokenizerTask.decode(outputs[0][len(inputs['input_ids'][0]):], skip_special_tokens=True)
     # print(ex['word'], generated_def)
@@ -238,7 +238,7 @@ def run_baseline_no_gd(def_task):
 
 def run_emb_gen(def_task, path):
     # config_args = extract_arguments_from_path(args.path)
-    fname_format = "definition_task_outputs/emb_gen_generations_masked_new_token_cls"
+    fname_format = "definition_task_outputs/emb_gen_generations_masked_new_token_temp08_top10"
     tokenizerMLM = AutoTokenizer.from_pretrained(path + "/tokenizerMLM", use_fast=False)
     tokenizerTask = LlamaTokenizer.from_pretrained(path + "tokenizerTask", use_fast=False, legacy=True)
     nonces = list(tokenizerTask.get_added_vocab().keys())
@@ -421,10 +421,10 @@ if __name__ == "__main__":
     def_task = load_from_disk("def_task_994")
     # def_task = def_task.map(replace_for_llama_baseline)
     # run_baseline(def_task, args.lr)
-    # path="model_checkpoints/layers/no_mp/llama/input_and_output/filtered/pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_4_8500"
+    path="model_checkpoints/layers/no_mp/llama/input_and_output/filtered/pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_4_8500"
     # path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/cls_agg/4_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_7_13500"
-    # run_emb_gen(def_task, path)
-    run_baseline_no_gd(def_task)
+    run_emb_gen(def_task, path)
+    # run_baseline_no_gd(def_task)
     # print("running Hice....")
     # run_hice(def_task)
     # print("Running Additive....")
