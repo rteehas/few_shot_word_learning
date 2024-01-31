0ea58882d4cb0df20e9170b07971a9028b06be97
Ryan Teehan <rsteehan@gmail.com>
2024-01-31 10:32:17 -0500

    fix vanilla

--- relational_eval.py
+++ relational_eval.py
@@ -403,7 +403,7 @@ def run_baseline(with_relation=True):
 if __name__ == "__main__":
     path="model_checkpoints/layers/no_mp/llama/input_and_output/filtered/pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_4_8500"
     # path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/redone_pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_2_9000"
-    main(path, let=True)
+    # main(path, let=True)
     run_vanilla()
     # print("running with relation=True")
     # run_baseline(True)
