ea7ce07848adf5458d0f4215865b8c4e1cdccedb
Ryan Teehan <rsteehan@gmail.com>
2024-03-06 20:59:02 -0500

    update path

--- definition_eval.py
+++ definition_eval.py
@@ -435,7 +435,7 @@ if __name__ == "__main__":
     elif args.model == "baseline_no_gd":
         run_baseline_no_gd(def_task)
     elif args.model == "emb_gen":
-        path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/redone_pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_4_16000"
+        path = "model_checkpoints/layers/no_mp/llama/input_and_output/filtered/redone_pile/layernorm/roberta-large/1_layers/last_1/32_batch_size/mean_agg/1_examples/lr_0.001/weight_decay_0.1/with_negatives_and_regression/distillation_weight_0.05_temp_3/output_embedding_cosine/checkpoints/checkpoint_7_28000"
         run_emb_gen(def_task, path)
     else:
         raise NotImplementedError
