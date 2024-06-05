8f05010e794baf3132529932e6936e86c7e87954
Ryan Teehan <rsteehan@gmail.com>
2024-06-05 14:55:08 -0400

    fix rome

--- ke.py
+++ ke.py
@@ -75,7 +75,7 @@ def rome_edit(editor, target_definition):
         target_new=target_new,
         subject=subject,
         keep_original_weight=True,
-        # return_orig_weights=True
+        return_orig_weights=True
     )
     # print(metrics)
     return edited_model, editor.tok, weights_copy
