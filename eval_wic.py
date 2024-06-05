73c3bc6a7ffa132c25c5e177177703c08e074118
Ryan Teehan <rsteehan@gmail.com>
2024-06-05 02:20:29 -0400

    fixing

--- eval_wic.py
+++ eval_wic.py
@@ -20,15 +20,15 @@ class CoLLEGeEmbeddingModel(nn.Module):
 
         self.emb_gen = EmbeddingGenerator(self.firstLM, 4096, num_layers, config=self.memory_config, use_pos=use_pos)
         
-        with torch.no_grad():
-            # firstLM_mean_embed = torch.mean(self.firstLM.get_output_embeddings().weight[:self.initial_first_ind, :], dim=0)
-            output_mean_embed = torch.mean(
-                self.secondLM.get_output_embeddings().weight.norm(dim=1))
-            # firstLM_std = torch.std(self.firstLM.get_output_embeddings().weight[:self.initial_first_ind, :], dim=0)
-            input_mean_embed = torch.mean(
-                self.secondLM.get_input_embeddings().weight.norm(dim=1))
+        # with torch.no_grad():
+        #     # firstLM_mean_embed = torch.mean(self.firstLM.get_output_embeddings().weight[:self.initial_first_ind, :], dim=0)
+        #     output_mean_embed = torch.mean(
+        #         self.secondLM.get_output_embeddings().weight.norm(dim=1))
+        #     # firstLM_std = torch.std(self.firstLM.get_output_embeddings().weight[:self.initial_first_ind, :], dim=0)
+        #     input_mean_embed = torch.mean(
+        #         self.secondLM.get_input_embeddings().weight.norm(dim=1))
 
-            self.emb_gen.init_weights(input_mean_embed, output_mean_embed)
+        #     self.emb_gen.init_weights(input_mean_embed, output_mean_embed)
 
         #     torch.register_buffer("firstLM_mean_embed", self.firstLM_mean_embed)
         #     torch.register_buffer("secondLM_mean_embed", self.secondLM_mean_embed)
