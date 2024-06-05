0578c5adf1a3a483de0a431e0fd4cad6252539b6
Ryan Teehan <rsteehan@gmail.com>
2024-06-04 23:06:54 -0400

    fix paths

--- ke.py
+++ ke.py
@@ -106,7 +106,7 @@ def eval_ke_baseline(ex, sents, defs, with_definition=False, with_prompt=False):
 def run_ke_baseline():
     args = get_arguments().parse_args()
     path = args.path
-    gre = load_from_disk("processed_kaplan_v0")
+    gre = load_from_disk("../processed_kaplan_v0")
     id = uuid.uuid4()
     subselection = gre.filter(lambda ex: "(i)" not in ex['QUESTION'])
     if args.defs != '':
@@ -204,8 +204,8 @@ def run_ke_baseline():
 
 def get_arguments():
     parser = ArgumentParser()
-    parser.add_argument("--sents", type=str, default='gre_examples_gpt4_v2.json')
-    parser.add_argument("--defs", type=str, default='gre_definitions_all.json')
+    parser.add_argument("--sents", type=str, default='../gre_examples_gpt4_v2.json')
+    parser.add_argument("--defs", type=str, default='../gre_definitions_all.json')
     parser.add_argument("--sent_version", type=str)
     parser.add_argument("--trials", type=int, default=1)
     parser.add_argument("--with_prompt", action="store_true")
