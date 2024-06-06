8965d083f3a4537cf875abf909e45f91dfa10655
Ryan Teehan <rsteehan@gmail.com>
2024-06-05 23:19:29 -0400

    take mean of multiple occurrences

--- eval_wic.py
+++ eval_wic.py
@@ -255,6 +255,8 @@ if __name__ == "__main__":
             def_str = "The word <nonce> is defined as {}".format(definition)
             with torch.no_grad():
                 ctx_hidden, def_hidden = predict_example(context, def_str, model, tokenizerMLM, tokenizerTask, new_token_idx)
+                if def_hidden.shape[0] > 1:
+                    def_hidden = torch.mean(def_hidden, dim=0, keepdim=True)
                 cat_embeds = torch.cat([ctx_hidden, def_hidden], dim=1)
 
             logits, loss = classifier(cat_embeds, labels=torch.tensor([label], device=device).unsqueeze(0))
