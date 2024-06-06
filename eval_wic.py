5c51f1131bd671fc8704108abc874953b24ba7b7
Ryan Teehan <rsteehan@gmail.com>
2024-06-05 23:14:15 -0400

    add for training cclassifier

--- eval_wic.py
+++ eval_wic.py
@@ -4,6 +4,10 @@ import json
 from tqdm import tqdm
 import torch.nn as nn
 import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+import wandb
+from torch.optim import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
 
 class CoLLEGeEmbeddingModel(nn.Module):
     def __init__(self, firstLM, num_new_tokens, layers, mask_token_id, memory_config, num_layers,
@@ -89,7 +93,19 @@ class CoLLEGeEmbeddingModel(nn.Module):
 
         return input_embeds, output_embeds, college_embeds
 
-
+class CoLLEGeWiCClassifier(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.classifier = nn.Linear(hidden_size * 2, 1)
+    
+    def forward(self, x, labels = None):
+        logits = self.classifier(x)
+        if labels is not None:
+            loss_fct = nn.BCEWithLogitsLoss()
+            loss = loss_fct(logits, labels.float())
+        else:
+            loss = None
+        return logits, loss
 # def predict_example(context, definition, model, tokenizer):
 #     ctx_tok = tokenizer([context], return_tensors = 'pt').to("cuda")
 #     def_tok = tokenizer([definition], return_tensors = 'pt').to("cuda")
@@ -105,10 +121,12 @@ def predict_example(context, definition, model, tokenizerMLM, tokenizerTask, new
     ctx_tok = tokenizerMLM([context], return_tensors = 'pt').to("cuda")
     def_tok = tokenizerMLM([definition], return_tensors = 'pt').to("cuda")
     
-    query_inputs = tokenizerTask([context, definition], return_tensors='pt', padding='longest')
+    query_inputs = tokenizerTask([context, definition], return_tensors='pt', padding='longest').to("cuda")
     # input_embeds, output_embeds, college_embeds = model.get_college_embeddings([ctx_tok, def_tok])
-    ctx_token_idx = torch.where(query_inputs['input_ids'][0] == new_token_idx)
-    def_token_idx = torch.where(query_inputs['input_ids'][0] == new_token_idx)
+    ctx_token_idx = torch.where(query_inputs['input_ids'][0] == new_token_idx)[0]
+    def_token_idx = torch.where(query_inputs['input_ids'][1] == new_token_idx)[0]
+    print(ctx_token_idx, def_token_idx)
+    print(query_inputs['input_ids'])
     labels = query_inputs['input_ids'].clone()
     batch = {
         "contexts": [ctx_tok, def_tok],
@@ -118,13 +136,27 @@ def predict_example(context, definition, model, tokenizerMLM, tokenizerTask, new
     }
     outputs = model(batch, output_hidden_states=True)
     ctx_new_token_hidden = outputs.hidden_states[-1][0, ctx_token_idx, :]
-    def_new_token_hidden = outputs.hidden_states[-1][1, def_token_idx :]
-    cos = nn.CosineSimilarity()
+    def_new_token_hidden = outputs.hidden_states[-1][1, def_token_idx, :]
+    # cos = nn.CosineSimilarity()
     # input_cos = cos(input_embeds[0], input_embeds[1])
     # output_cos = cos(output_embeds[0], output_embeds[1])
     # college_cos = cos(college_embeds[0], college_embeds[1])
 
-    return cos(ctx_new_token_hidden, def_new_token_hidden)
+    return ctx_new_token_hidden, def_new_token_hidden
+
+
+def compute_metrics(preds, labels):
+    preds = np.array(preds)
+    labels = np.array(labels)
+    res = (preds == labels).astype(type(labels[0]))
+    precision, r, f1, _ = precision_recall_fscore_support(y_true=labels, y_pred=preds, average='binary')
+    acc = res.mean()
+    return {
+        "acc": acc,
+        "F_1": f1,
+        "P": precision,
+        "R": r,
+    }
 
 
 
@@ -142,7 +174,9 @@ if __name__ == "__main__":
     nonces = list(tokenizerTask.get_added_vocab().keys())
     mask_token_id = tokenizerMLM.mask_token_id
     layers=[-1]
-
+    epochs = 10
+    lr = 1e-4
+    weight_decay = 0.05
     # model = CoLLEGeEmbeddingModel(
     #     firstLM=firstLM,
     #     num_new_tokens=1,
@@ -156,90 +190,186 @@ if __name__ == "__main__":
     model = MorphMemoryModelLLAMA(firstLM, secondLM, len(nonces), layers, mask_token_id, memory_config, 1, None).to(device)
     model.emb_gen.load_state_dict(torch.load(path + "/pytorch_model.bin"))
     model = model.to(device)
+
+    new_token_idx = len(tokenizerTask) - 1
     # tokenizerMLM.add_tokens(["<nonce>"])
     print(model.first_list)
     model.eval()
     thresholds = np.linspace(0, 1, num=100).tolist()
-    results = {}
-    for e in ["input", "output", "college"]:
-        results[e] = {"true pos": [0 for i in range(len(thresholds))],
-                      "false pos": [0 for i in range(len(thresholds))],
-                      "true neg": [0 for i in range(len(thresholds))],
-                      "false neg": [0 for i in range(len(thresholds))]}
+
+    classifier = CoLLEGeWiCClassifier(secondLM.config.hidden_size).to(device)
+    for p in model.parameters():
+        p.requires_grad = False
+
+    run_name = "classifier_epochs={}_lr={}_weight_decay={}".format(epochs, lr, weight_decay)
+    run = wandb.init(project="few_shot_wic",
+                     name=run_name
+                    )
     
+    # results = {}
+    # for e in ["input", "output", "college"]:
+    #     results[e] = {"true pos": [0 for i in range(len(thresholds))],
+    #                   "false pos": [0 for i in range(len(thresholds))],
+    #                   "true neg": [0 for i in range(len(thresholds))],
+    #                   "false neg": [0 for i in range(len(thresholds))]}
+    # results = {"true pos": [0 for i in range(len(thresholds))],
+    #                   "false pos": [0 for i in range(len(thresholds))],
+    #                   "true neg": [0 for i in range(len(thresholds))],
+    #                   "false neg": [0 for i in range(len(thresholds))]}
     
 
     train_folder = Path('wic/wic_tsv/data/en/Training')
+    dev_folder = Path('wic/wic_tsv/data/en/Training')
     contexts, target_inds, hypernyms, definitions, labels = dp.read_wic_tsv(wic_tsv_folder=train_folder)
     print("Ratio of positives:", sum(labels) / len(labels))
+    dev_contexts, dev_target_inds, dev_hypernyms, dev_definitions, dev_labels = dp.read_wic_tsv(wic_tsv_folder=dev_folder)
+
+    opt = AdamW(params=classifier.parameters(),
+                lr=lr,
+                weight_decay=weight_decay)
+    
+    scheduler = get_cosine_schedule_with_warmup(opt, 
+                                                num_warmup_steps = 300,
+                                                num_training_steps = len(contexts) * epochs)
 
     for i, (context, target_id, definition) in enumerate(zip(contexts, target_inds, definitions)):
         word = context.split()[target_id]
         
         contexts[i] = re.sub(r"\b({})\b".format(word), "<nonce>", context, flags=re.I)
         definitions[i] = re.sub(r"\b({})\b".format(word), "<nonce>", definition, flags=re.I)
-
-    for context, definition, label in tqdm(zip(contexts, definitions, labels), total=len(contexts)):
-        print(context, definition)
-        def_str = "The word <nonce> is defined as {}".format(definition)
-        sims = predict_example(context, def_str, model, tokenizerMLM)
-
-        ex_results = []
-        for j, threshold in enumerate(thresholds):
-            for i, e in enumerate(["input", "output", "college"]):
-                pred = int(sims[i].item() >= threshold)
-                print(sims[i], sims[i].item(), threshold, pred, label)
-                if pred == label:
-                    if pred == 1:
-                        results[e]["true pos"][j] += 1
-                    elif pred == 0:
-                        results[e]["true neg"][j] += 1
-                
-                else:
-                    if pred == 1:
-                        results[e]["false pos"][j] += 1
-                    elif pred == 0:
-                        results[e]["false neg"][j] += 1
     
-    for key in results:
-        print("Results for {} Embeddings".format(key.upper()))
-        emb_results = results[key]
-        precisions = []
-        accs = []
-        f1s = []
-        recalls = []
+    for i, (context, target_id, definition) in enumerate(zip(dev_contexts, dev_target_inds, dev_definitions)):
+        word = context.split()[target_id]
+        
+        dev_contexts[i] = re.sub(r"\b({})\b".format(word), "<nonce>", context, flags=re.I)
+        dev_definitions[i] = re.sub(r"\b({})\b".format(word), "<nonce>", definition, flags=re.I)
+    
+    global_step = 0
+    for epoch in epochs:
+        train_predictions = []
+        train_labels = []
+        train_loss = 0
+        classifier.train()
+        for context, definition, label in tqdm(zip(contexts, definitions, labels), total=len(contexts)):
+            print(context, definition)
+            def_str = "The word <nonce> is defined as {}".format(definition)
+            with torch.no_grad():
+                ctx_hidden, def_hidden = predict_example(context, def_str, model, tokenizerMLM, tokenizerTask, new_token_idx)
+                cat_embeds = torch.cat([ctx_hidden, def_hidden], dim=1)
 
-        for i in range(len(thresholds)):
-            true_pos = emb_results['true pos'][i]
-            true_neg = emb_results['true neg'][i]
-            false_pos = emb_results['false pos'][i]
-            false_neg = emb_results['false neg'][i]
-            try:
-                precision = true_pos / (true_pos + false_pos)
-            except ZeroDivisionError:
-                precision = 0
-            
-            try:
-                recall = true_pos / (true_pos + false_neg)
-            except ZeroDivisionError:
-                recall = 0
-            try:   
-                f1 = 2 * (precision * recall) / (precision + recall)
-            except ZeroDivisionError:
-                f1 = 0
-            acc = (true_pos + true_neg) / len(contexts)
+            logits, loss = classifier(cat_embeds, labels=torch.tensor([label], device=device))
+            wandb.log({"train loss": loss.item(),
+                       "global step": global_step})
+            loss.backward()
+            opt.step()
+            scheduler.step()
+            opt.zero_grad()
+            model.zero_grad()
 
-            precisions.append(precision)
-            accs.append(acc)
-            f1s.append(f1)
-            recalls.append(recall)
+            preds = torch.flatten((logits >= 0.5).int()).detach().tolist()
+            train_predictions += preds
+            train_loss += loss.detach().float()
+            train_labels.append(label)
 
-        print("Thresholds: ", thresholds)
-        print("Precisions: ", precisions)
-        print("Recalls: ", recalls)
-        print("F1 Scores: ", f1s)
-        print("Accuracies: ", accs)
+            global_step += 1
+        
+        test_predictions = []
+        test_labels = []
+        test_loss = 0
+        classifier.eval()
+        for dev_context, dev_definition, dev_label in tqdm(zip(dev_contexts, dev_definitions, dev_labels), total=len(contexts)):
+            def_str = "The word <nonce> is defined as {}".format(dev_definition)
+            with torch.no_grad():
+                ctx_hidden, def_hidden = predict_example(dev_context, def_str, dev_definition, model,
+                                                         tokenizerMLM, tokenizerTask, new_token_idx)
+                cat_embeds = torch.cat([ctx_hidden, def_hidden], dim=1)
+                logits, loss = classifier(cat_embeds, labels=torch.tensor([label], device=device))
+                test_preds = torch.flatten((logits >= 0.5).int()).detach().tolist()
+                test_loss += loss.detach().float()
+                test_predictions += test_preds
+                test_labels.append(dev_label)
+        
 
-        with open("college_wic_results.json", 'w') as fp:
-            json.dump(results, fp)
+        avg_test_loss = test_loss / len(dev_contexts)
+        avg_train_loss = train_loss / len(contexts)
+        train_metrics = compute_metrics(train_predictions, train_labels)
+        test_metrics = compute_metrics(test_predictions, test_labels)
+
+        epoch_log_dict = {
+            "average train loss": avg_train_loss,
+            "average test loss": avg_test_loss,
+            "epoch": epoch
+        }
+
+        for key in train_metrics:
+            epoch_log_dict["train_{}".format(key)] = train_metrics[key]
+        
+        for key in test_metrics:
+            epoch_log_dict["test_{}".format(key)] = train_metrics[key]
+        
+        wandb.log(epoch_log_dict)
+
+            # ex_results = []
+            # for j, threshold in enumerate(thresholds):
+            #     cos = nn.CosineSimilarity()
+            #     sim = cos(ctx_hidden, def_hidden)
+            #     if len(sim) > 1:
+            #         pred = int(sim.max().item() >= threshold)
+            #     else:
+            #         pred = int(sim.item() >= threshold)
+            #     # int(sims[i].item() >= threshold)
+            #     print(sim, threshold, pred, label)
+            #     if pred == label:
+            #         if pred == 1:
+            #             results["true pos"][j] += 1
+            #         elif pred == 0:
+            #             results["true neg"][j] += 1
+                
+            #     else:
+            #         if pred == 1:
+            #             results["false pos"][j] += 1
+            #         elif pred == 0:
+            #             results["false neg"][j] += 1
+    
+    # for key in results:
+    #     print("Results for {} Embeddings".format(key.upper()))
+    #     emb_results = results[key]
+    # precisions = []
+    # accs = []
+    # f1s = []
+    # recalls = []
+
+    # for i in range(len(thresholds)):
+    #     true_pos = results['true pos'][i]
+    #     true_neg = results['true neg'][i]
+    #     false_pos = results['false pos'][i]
+    #     false_neg = results['false neg'][i]
+    #     try:
+    #         precision = true_pos / (true_pos + false_pos)
+    #     except ZeroDivisionError:
+    #         precision = 0
+        
+    #     try:
+    #         recall = true_pos / (true_pos + false_neg)
+    #     except ZeroDivisionError:
+    #         recall = 0
+    #     try:   
+    #         f1 = 2 * (precision * recall) / (precision + recall)
+    #     except ZeroDivisionError:
+    #         f1 = 0
+    #     acc = (true_pos + true_neg) / len(contexts)
+
+    #     precisions.append(precision)
+    #     accs.append(acc)
+    #     f1s.append(f1)
+    #     recalls.append(recall)
+
+    # print("Thresholds: ", thresholds)
+    # print("Precisions: ", precisions)
+    # print("Recalls: ", recalls)
+    # print("F1 Scores: ", f1s)
+    # print("Accuracies: ", accs)
+
+    # with open("college_wic_results.json", 'w') as fp:
+    #     json.dump(results, fp)
     
